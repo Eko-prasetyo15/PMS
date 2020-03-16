99d80e3191829d106990d447826441d42dade663
Eko-Prasetyo15 <prasetyoeko822@gmail.com>
2020-03-16 20:44:50 +0700

    login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  res.send(req.body);
 });
 
 module.exports = router;
