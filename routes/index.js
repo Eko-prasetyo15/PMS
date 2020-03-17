286adffd69191ab1902055bce597074b961177bd
Eko-Prasetyo15 <prasetyoeko822@gmail.com>
2020-03-17 18:48:29 +0700

    10%

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('login');
 });
 
-router.post('/login', function(req, res, next) {
-  res.send(req.body);
+router.get('/project', function(req, res, next) {
+  res.render('project', {title: "web"});
 });
+
+router.post('/login', function(req, res, next) {
+  res.redirect('project');
+});
+
+router.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/login');
+      }
+    });
+  }
+});
+
+router.get('/users', function(req, res, next) {
+  res.render('Users');
+});
+
+router.post('/project', function(req, res, next) {
+  res.redirect('Users');
+});
+
+router.get('/project/add', function(req, res, next) {
+  res.render("add")
+});
+
+
 
 module.exports = router;
